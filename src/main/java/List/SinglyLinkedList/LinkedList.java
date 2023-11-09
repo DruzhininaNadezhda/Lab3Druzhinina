b4d57ab51c780fa75ff3efdd79a67b79b517d22c
DruzhininaNadezhda <wulvjxe@gmail.com>
2023-11-09 13:50:47 +0300

    1) + Использование в имени класса цифр крайне нежелательно. 2) + Метод encrypt() перебирает все элементы кроме последнего. 3) + Метод toString() изменяет значение переменной класса head, что приводит к изменению структуры элементов связного списка. 4) + Метод delete(char data), если удаляемое значение находится в голове, то удаление не происходит. 5) + Метод getFirstDelete() проверять на null необходимо саму переменную head и если она не null, то head=head.next. Также обязательно нужно проверять tail на равенство head. 6) Не очень поняла, как это сделать, сделала метод, который печатает хвост, и хвост везде находит. "Практически во всех методах, изменяющих переменную head, вы забываете проверять tail. Например, если мы в новом списке добавим значение в начало, то хвост у нас останется пустым."

--- src/main/java/List/SinglyLinkedList/LinkedList.java
+++ src/main/java/List/SinglyLinkedList/LinkedList.java
@@ -1,7 +1,7 @@
 package List.SinglyLinkedList;
 
 
-public class List1 implements Interface {
+public class LinkedList implements Interface {
 
     private ListElement head;
     private ListElement tail;
@@ -9,9 +9,9 @@ public class List1 implements Interface {
     @Override
     public void addFirst(char data) {
         //проверка на пустоту и создание нового списка с головой из заданого элемента
-        ListElement a = new ListElement(data, null);
+        ListElement  a = new ListElement (data, null);
         if (head == null) {
-            head = a;
+            head = tail = a;
         } else {
             // голова делает шаг назад
             a.next = head;
@@ -28,11 +28,7 @@ public class List1 implements Interface {
     @Override
     public char getFirstDelete() {
         char g = getFirst();
-        if (head.next != null) {
-            head = head.next;
-        } else {
-            head = null;
-        }
+       delete(g);
         return g;
     }
 
@@ -77,12 +73,12 @@ public class List1 implements Interface {
             ListElement node = head;
             //цикл
             while (node.next != null) {
+                //значение в голове
+                if (head.data == data) {
+                    head = head.next;
+                }
                 //соответствие значений
                 if (node.next.data == data) {
-                    //значение в голове
-                    if (head.data == data) {
-                        head = head.next;
-                    }
                     //если это последние значение
                     if (node.next == tail) {
                         tail = node;
@@ -124,7 +120,7 @@ public class List1 implements Interface {
             ListElement node = head;
             int n =1;
             //цикл
-            while (node.next != null) {
+            while (node != null) {
                 node.data = (char) (node.data + n);
                 // шаг цикла
                 node = node.next;
@@ -137,28 +133,32 @@ public class List1 implements Interface {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
-        while (head != null) {
-            sb.append(head.data);
-            head = head.next;
+        ListElement node = head;
+        while (node!= null) {
+            sb.append(node.data);
+            node = node.next;
         }
         sb.append("]");
         return sb.toString();
     }
     public static void main(String[] args) {
-       List1 list = new List1();
+       LinkedList list = new LinkedList();
         list.addEnd('l');
-        list.addFirst('a');
-        list.addFirst('v');
-        list.addFirst('a');
+        //list.addFirst('a');
+        //list.addFirst('v');
+        //list.addFirst('j');
         list.addEnd('a');
         list.addFirst('j');
-        list.addEnd('b');
+        //list.addEnd('b');
+        System.out.println(list);
         //System.out.println(list.getFirstDelete());
-        //System.out.println(list.getEndDelete());
+        //System.out.println(list.getEnd());
         //list.delete('a');
-        System.out.println(list.yesOrNo('b'));
-        //list.encrypt();
+        //System.out.println(list.yesOrNo('b'));
+        //System.out.println(list);
+        list.encrypt();
         //list.emptyList();
         System.out.println(list);
+        System.out.println(list.tail.print());
     }
 }
